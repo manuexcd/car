6daaebf830552626b8e9c5d12d36e165902e0155
Manuel Lara <manuexcd@gmail.com>
2018-09-10 13:16:58 +0200

    Rest template config

--- src/main/java/com/spring/car/CarApplication.java
+++ src/main/java/com/spring/car/CarApplication.java
@@ -13,12 +13,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableMongoRepositories
 public class CarApplication {
 	
-	@Bean
-	@LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarApplication.class, args);
 	}
